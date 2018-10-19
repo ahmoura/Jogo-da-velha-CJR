97595727d7630db1fcfa39cf5b22534ea8c1e383
ArturHugo <arturpereira@cjr.org.br>
2018-10-18 21:06:38 -0300

    Adicionada pasta de imagens e js modificado para mudar o bg do quadro para uma das imagens. Adicionada a verificacao de velha na funcao vitoria.

--- js/jogo.js
+++ js/jogo.js
@@ -1,12 +1,12 @@
-var turno = 1;
-var campo = [9];
-var id_vec = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
-var ganhou = 0;
-
+let turno = 1;
+let campo = [9];
+let id_vec = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
+let ganhou = 0;
+let jogadas = 0;
 resetar();
 
 function vitoria() {
-    if ((campo[0] == campo[1]) && (campo[1] == campo[2]) && (campo[1] != 0)) {
+    if ((campo[0] == campo[1]) && (campo[1] == campo[2]) && (campo[0] != 0)) {
         if (campo[0] == 1) {
             alert("X ganhou!");
         }
@@ -51,7 +51,7 @@ function vitoria() {
         }
         ganhou = 1;
     }
-    else if ((campo[2] && campo[5]) && (campo[5] && campo[8]) && (campo[2] != 0)) {
+    else if ((campo[2] == campo[5]) && (campo[5] == campo[8]) && (campo[2] != 0)) {
         if (campo[2] == 1) {
             alert("X ganhou!");
         }
@@ -82,26 +82,33 @@ function vitoria() {
     if (ganhou == 1) {
         resetar();
     }
+    else if (jogadas == 9) {
+        alert("Deu velha!");
+        resetar();
+    }
 }
 
 function resetar() {
     for (i = 0; i < 9; i++) {
         campo[i] = 0;
-        ganhou = 0;
-        document.getElementById(id_vec[i]).innerHTML = "";
+        document.getElementById(id_vec[i]).style.backgroundImage = "none";
     }
+    turno = 1;
+    ganhou = 0;
+    jogadas = 0;
 }
 
 function jogada(id) {
     if (campo[id_vec.indexOf(id)] == 0) {
         if (turno == 1) {
-            document.getElementById(id).innerHTML = "<p class=\"jogo_p jogo_x\">X</p>";
+            document.getElementById(id).style.backgroundImage = "url('../img/x.png')";
         }
         else {
-            document.getElementById(id).innerHTML = "<p class=\"jogo_p jogo_o\">O</p>";
+            document.getElementById(id).style.backgroundImage = "url('../img/o.png')";
         }
         campo[id_vec.indexOf(id)] = turno;
         turno = turno * (-1);
+        jogadas++;
         vitoria();
     }
 }
