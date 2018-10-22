b105df3b8fff28f45e8ba1ef9646c135049acbab
ArturHugo <arturpereira@cjr.org.br>
2018-10-22 13:20:44 -0300

    Criado o placar do jogo.

--- js/jogo.js
+++ js/jogo.js
@@ -1,80 +1,83 @@
-let turno = 1;
-let campo = [9];
+let turno = 1;      /* Verifica o turno ativo: 1 para X e 0 para O. */
+let campo = [9];    /* Vetor de cada tile do tabuleiro */
+/* Vetor com o id de cada tile */
 let id_vec = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"];
-let ganhou = 0;
-let jogadas = 0;
+let ganhou = 0;     /* Verifica se o alguem ganhou */
+let jogadas = 0;    /* Verifica o numero de jogadas no jogo atual (usado para conferir se deu velha) */
 resetar();
 
+
+/* Funcoes basicas do jogo.*/
 function vitoria() {
     if ((campo[0] == campo[1]) && (campo[1] == campo[2]) && (campo[0] != 0)) {
         if (campo[0] == 1) {
-            alert("X ganhou!");
+            incrementa_vitoria(1);
         }
         else {
-            alert("O ganhou!");
+            incrementa_vitoria(0);
         }
         ganhou = 1;
     }
     else if ((campo[3] == campo[4]) && (campo[4] == campo[5]) && (campo[3] != 0)) {
         if (campo[3] == 1) {
-            alert("X ganhou!");
+            incrementa_vitoria(1);
         }
         else {
-            alert("O ganhou!");
+            incrementa_vitoria(0);
         }
         ganhou = 1;
     }
     else if ((campo[6] == campo[7]) && (campo[7] == campo[8]) && (campo[6] != 0)) {
         if (campo[6] == 1) {
-            alert("X ganhou!");
+            incrementa_vitoria(1);
         }
         else {
-            alert("O ganhou!");
+            incrementa_vitoria(0);
         }
         ganhou = 1;
     }
     else if ((campo[0] == campo[3]) && (campo[3] == campo[6]) & (campo[0] != 0)) {
         if (campo[0] == 1) {
-            alert("X ganhou!");
+            incrementa_vitoria(1);
         }
         else {
-            alert("O ganhou!");
+            incrementa_vitoria(0);
         }
         ganhou = 1;
     }
     else if ((campo[1] == campo[4]) && (campo[4] == campo[7]) && (campo[1] != 0)) {
         if (campo[1] == 1) {
-            alert("X ganhou!");
+            incrementa_vitoria(1);
         }
         else {
-            alert("O ganhou!");
+            incrementa_vitoria(0);
         }
         ganhou = 1;
     }
     else if ((campo[2] == campo[5]) && (campo[5] == campo[8]) && (campo[2] != 0)) {
         if (campo[2] == 1) {
-            alert("X ganhou!");
+            incrementa_vitoria(1);
         }
         else {
-            alert("O ganhou!");
+            incrementa_vitoria(0);
         }
         ganhou = 1;
     }
     else if ((campo[0] == campo[4]) && (campo[4] == campo[8]) && (campo[0] != 0)) {
         if (campo[0] == 1) {
-            alert("X ganhou!");
+            incrementa_vitoria(1);
         }
         else {
-            alert("O ganhou!");
+            incrementa_vitoria(0);
         }
         ganhou = 1;
     }
     else if ((campo[2] == campo[4]) && (campo[4] == campo[6]) && (campo[2] != 0)) {
         if (campo[2] == 1) {
-            alert("X ganhou!");
+            incrementa_vitoria(1);
         }
         else {
-            alert("O ganhou!");
+            incrementa_vitoria(0);
         }
         ganhou = 1;
     }
@@ -109,5 +112,26 @@ function jogada(id) {
         turno = turno * (-1);
         jogadas++;
         vitoria();
+    }
+}
+
+
+
+/* Contador de vitorias.*/
+let win_count = 0;
+/* Contador de derrotas. */
+let lose_count = 0;
+
+/* Funcao para incrementar vitorias ou derrotas. */
+function incrementa_vitoria(quem_ganhou) {
+    if(quem_ganhou==1){
+        win_count++;
+        document.getElementById("conta_vitorias").innerHTML = win_count;
+        alert("X ganhou!");
+    }
+    else if(quem_ganhou==0){
+        lose_count++;
+        document.getElementById("conta_derrotas").innerHTML = lose_count;
+        alert("O ganhou!");
     }
 }
